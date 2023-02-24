0a6f8fd33640b2e79e889a952a3003bc9b482046
krystal <krystal961020@gmail.com>
2023-02-24 19:39:49 +0800

    update annotation

--- station-modules/station/src/main/java/com/swms/station/api/ApiController.java
+++ station-modules/station/src/main/java/com/swms/station/api/ApiController.java
@@ -5,16 +5,12 @@ import com.swms.common.http.Response;
 import com.swms.station.executor.HandlerExecutor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * receive operator's operations
  */
-@Controller
+@RestController
 @RequestMapping("api")
 public class ApiController {
 
