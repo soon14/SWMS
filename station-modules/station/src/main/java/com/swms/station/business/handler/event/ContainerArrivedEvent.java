299a7d1d165b4ae03c562522205da00911a038a5
kang.nie <kang.nie@hairoutech.com>
2023-03-15 18:24:44 +0800

    add new field.

--- station-modules/station/src/main/java/com/swms/station/business/handler/event/ContainerArrivedEvent.java
+++ station-modules/station/src/main/java/com/swms/station/business/handler/event/ContainerArrivedEvent.java
@@ -1,5 +1,6 @@
 package com.swms.station.business.handler.event;
 
+import com.swms.wms.api.warehouse.constants.WorkLocationTypeEnum;
 import lombok.Data;
 
 @Data
@@ -11,10 +12,10 @@ public class ContainerArrivedEvent {
     private String robotType;
     private Integer level;
     private Integer bay;
-
     private String locationCode;
 
     private String workLocationCode;
+    private WorkLocationTypeEnum workLocationType;
 
     private String groupCode;
 }
