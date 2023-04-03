cd5b90615fd6cb835e40865804ba18d9009aaeb4
Krystal <krystal961020@gmail.com>
2023-04-03 18:52:57 +0800

    update config

--- server/wms-server/src/main/java/com/swms/wms/WmsApplication.java
+++ server/wms-server/src/main/java/com/swms/wms/WmsApplication.java
@@ -1,9 +1,13 @@
 package com.swms.wms;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.swms"})
+@SpringBootApplication(scanBasePackages = {"com.swms.**"})
+@EnableDubbo(scanBasePackages = "com.swms")
+@MapperScan("com.swms.**.mapper")
 public class WmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(WmsApplication.class, args);
